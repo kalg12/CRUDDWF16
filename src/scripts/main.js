9d050b4b07e1d2b24ec2e5e54b726f0ab42d5406
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-06-07 19:48:22 -0600

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+console.log("Probando...");
+
+localStorage.setItem("UCAMP", "DWF 16");
+localStorage.setItem(
+  "Los que me acompañaron en esta sesion",
+  "[Miguel, Juan Carlos]"
+);
+localStorage.setItem("Computadora", "{marca:HP, medida:17 pulgadas}");
